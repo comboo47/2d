976d828390c932f4da3ff3ceeb53d3e8b3c02bcb
LiuNong <376586656@qq.com>
2024-06-03 15:11:27 +0800

    子弹和武器优化

--- yongzhedouelong/Scripts/bullet.gd
+++ yongzhedouelong/Scripts/bullet.gd
@@ -5,12 +5,12 @@ extends RigidBody2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$AnimatedSprite2D.play("default")
-	
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
+	$".".look_at($".".linear_velocity)
 	pass
 func _releaseSelf():
 	$".".queue_free()
@@ -18,5 +18,10 @@ func _releaseSelf():
 func _on_bullet_hit(body:Node):
 	if body.has_method("_beHurt"):
 		body._beHurt(damage)
-	_releaseSelf()
+	$Timer.start()
 		
+
+
+func _on_body_entered(body):
+	_releaseSelf()
+	pass # Replace with function body.
