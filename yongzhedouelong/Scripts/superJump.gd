5a159e4bd3d130b333a9b8df2df0430c6de8d357
hopeless <376586656@qq.com>
2024-06-06 02:54:20 +0800

    新增和敌人的碰撞效果

--- yongzhedouelong/Scripts/superJump.gd
+++ yongzhedouelong/Scripts/superJump.gd
@@ -3,6 +3,7 @@ extends Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	$AnimatedSprite2D.play("default")
 	pass # Replace with function body.
 
 
@@ -12,9 +13,9 @@ func _ready():
 
 func _on_body_entered(body):
 	if body.is_in_group("Player"):
-		var by = body.position.y
-		var sy = $".".position.y
-		var bv = body.velocity.y
-		if by <= sy && bv > 0:
+		if body.position.y <= $".".position.y && body.velocity.y > 0:
 			body.velocity.y = jumpVelocity
+			$AnimatedSprite2D.play("trigger")
+			await $AnimatedSprite2D.animation_finished
+			$AnimatedSprite2D.play("default")
 	pass # Replace with function body.
