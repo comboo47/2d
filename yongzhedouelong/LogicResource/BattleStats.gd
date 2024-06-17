f860a0a872b4170a172508f5cb93f09ad3946488
hopeless <376586656@qq.com>
2024-06-17 22:55:11 +0800

    敌人分离

---
+++ yongzhedouelong/LogicResource/BattleStats.gd
@@ -0,0 +1,37 @@
+class_name BattleStats
+extends Node
+
+var curHp:float
+var curMaxHp:float
+
+var atk:float
+var atkAdd:float
+var atkMul:float
+
+var def:float
+var defAdd:float
+var defMul:float
+
+var speed:float
+var speedAdd:float
+var speedMul:float
+
+var atkTime:float
+var atkTimeAdd:float
+var atkTimeMul:float
+
+@export var Stats:BaiscStats
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	curMaxHp = Stats.MaxHealth
+	curHp = curMaxHp
+	atk = Stats.AttackPoint
+	def = Stats.DefencePoint
+	speed = Stats.MoveSpeed
+	atkTime = Stats.AttackTime
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
---
+++ yongzhedouelong/prefab/Enemy/base_enemy.gd
@@ -0,0 +1,44 @@
+extends CharacterBody2D
+
+var isDead:bool = false
+
+
+@export var EnemyID = 1001
+@onready var move_component:MoveComponent = $MoveComponent as MoveComponent
+@onready var visible_on_screen_notifier_2d:VisibleOnScreenNotifier2D = $VisibleOnScreenNotifier2D as VisibleOnScreenNotifier2D
+@onready var animated_sprite_2d:AnimatedSprite2D = $AnimatedSprite2D as AnimatedSprite2D
+@onready var body_area:BodyArea = $BodyArea as BodyArea
+@onready var hurt_display_component:HurtDisplayComponent = $HurtDisPlay as HurtDisplayComponent
+@onready var stats:BattleStats = $BattleStats as BattleStats
+
+
+func _ready():
+	visible_on_screen_notifier_2d.screen_exited.connect(queue_free)
+
+func getID():
+	return EnemyID
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+
+func hitDisplay():
+	animated_sprite_2d.modulate.a = (float(stats.curHp)/float(stats.curMaxHp))
+	hurt_display_component.hitDisplay()
+
+func _beHurt(dmg):
+	stats.curHp = stats.curHp - dmg
+	if(stats.curHp <= 0):
+		if !isDead:
+			setDead()
+	hitDisplay()
+func setDead():
+	isDead = true
+	$".".set_collision_layer_value(2,false)
+	if get_node("/root/DropManager"):
+		get_node("/root/DropManager").dropPoker(self)
+	var tween = create_tween()
+	
+	tween.set_trans(Tween.TRANS_BACK)
+	tween.set_ease(Tween.EASE_OUT)
+	tween.tween_property($".","scale",Vector2(),0.2)
+	await tween.finished
+	$".".queue_free()
+	pass
